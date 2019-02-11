275222dcc99da5bf790f189db27364eb9005c99a
Brendan <bcarney02@gmail.com>
2019-02-10 18:16:27 -0600

    Initial

---
+++ config/website.js
@@ -0,0 +1,28 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
+  siteTitle: "Brendan's Blog", // Navigation and Site Title
+  siteTitleAlt: 'A quickly made blog', // Alternative Site title for SEO
+  siteTitleShort: "I'm not sure what to put for all these right now", // short_name for manifest
+  siteUrl: 'https://brendan.im', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription: 'This is (hopefully) the quickest way to make a Gatsby blog',
+  author: 'Brendan Carney', // Author for schemaORGJSONLD
+  organization: 'Just Me',
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@brendanrc2', // Twitter Username
+  ogSiteName: 'brendan.im A blog made in a short amount of time', // Facebook Site Name
+  ogLanguage: 'en_US',
+  googleAnalyticsID: '',
+
+  // Manifest and Progress color
+  themeColor: '#5348FF',
+  backgroundColor: '#2b2e3c',
+
+  // Social component
+  twitter: 'https://twitter.com/brendanrc2/',
+  twitterHandle: '@brendanrc2',
+  github: 'https://github.com/brendancarney/',
+  linkedin: '',
+}
